66b63a475fff15851b32ab81ef4d5e38812a0ba0
Alexander Murphy <amurphy345@gmail.com>
2018-12-09 17:59:08 -0700

    adding mapping functions

--- ApolloUnitTestingDemo/CountryStore.swift
+++ ApolloUnitTestingDemo/CountryStore.swift
@@ -8,10 +8,22 @@ enum World {
     }
     
     struct Country {
+        struct Language {
+            let name: String
+        }
         
+        struct Continent {
+            let name: String
+        }
+        
+        let name: String
+        let currency: String
+        let phone: String
+        let emoji: String
+        let languages: [Language]
+        let continent: Continent
     }
 }
-
 
 protocol ApolloClientInterface {
     @discardableResult func fetch<Query: GraphQLQuery>(query: Query,
@@ -32,17 +44,65 @@ protocol WorldStoreInterface {
 extension WorldStoreInterface {
     func fetchAllCountries(cachePolicy: CachePolicy = .fetchIgnoringCacheData,
                            completion: @escaping ((Result<[World.Country], World.StoreError>) -> Void)) {
+       
         let query = WorldQuery()
         let queue = DispatchQueue.global(qos: .default)
         
         let resultHandler: OperationResultHandler<WorldQuery> = { (result, error) in
+
+            guard error == nil else {
+                completion(.failure(.network))
+                return
+            }
             
+            guard let countries = result?.data?.countries?.compactMap(Self.makeCountry(from:)) else {
+                completion(.failure(.parsing))
+                return
+            }
+            
+            completion(.success(countries))
         }
         
         client.fetch(query: query,
                      cachePolicy: cachePolicy,
                      queue: queue,
                      resultHandler: resultHandler)
+    }
+    
+    private static func makeCountry(from country: WorldQuery.Data.Country?) -> World.Country? {
+        guard
+            let name = country?.name,
+            let currency = country?.currency,
+            let phone = country?.phone,
+            let emoji = country?.emoji,
+            let continent = Self.makeContinent(from: country?.continent),
+            let languages = country?.languages?.compactMap(Self.makeLanguages(from:))
+                else {
+                    return nil
+        }
+        
+        return World.Country(name: name,
+                             currency: currency,
+                             phone: phone,
+                             emoji: emoji,
+                             languages: languages,
+                             continent: continent)
+    }
+    
+    private static func makeContinent(from continent: WorldQuery.Data.Country.Continent?) -> World.Country.Continent? {
+        guard let name = continent?.name else {
+            return nil
+        }
+        
+        return World.Country.Continent(name: name)
+    }
+    
+    private static func makeLanguages(from language: WorldQuery.Data.Country.Language?) -> World.Country.Language? {
+        guard let name = language?.name else {
+            return nil
+        }
+        
+        return World.Country.Language(name: name)
     }
 }
 
